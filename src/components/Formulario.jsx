b572b6f896907088c9c6ce75d28d9d1eff0105f5
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-04-06 15:47:54 -0500

    validando formularios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,30 @@
 import { Fragment } from 'react';
 import { MARCAS, YEARS, PLANES } from '../constants'
 import useCotizador from '../hooks/useCotizador';
+import Error from './Error';
 
 const Formulario = () => {
 
-    const { datos, handleChangeDatos } = useCotizador();
+    const { datos, handleChangeDatos, error,setError } = useCotizador();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(datos);
+
+        if(Object.values(datos).includes("")){
+            setError("Todos los campos son obligatorios");
+            return;
+        }
+
+        setError('');
+    }
 
     return ( 
         <>
-
-            <form>
+            {error && <Error/>}
+            <form
+                onSubmit={handleSubmit}
+            >
                 <div className="my-5">
                     <label htmlFor="" className='block mb-3 font-bold text-gray-400 uppercase'>
                         Marca
