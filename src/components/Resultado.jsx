616ad543d3bd96fdfe71bb4c4544f7dcc549da35
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-04-25 14:47:29 -0500

    finalizando el componente de resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -7,7 +7,7 @@ const Resultado = () => {
     const { marca, year, plan } = datos;
 
     const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
-
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
 
     if(resultado === 0) return null;
 
@@ -19,6 +19,18 @@ const Resultado = () => {
             <p className="my-2">
                 <span className="font-bold">Marca:</span>
                 {nombreMarca.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Plan:</span>
+                {nombrePlan.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Año del Auto:</span>
+                {year}
+            </p>
+            <p className="my-2 text-2xl">
+                <span className="font-bold">Total Cotizacion:</span>
+                {resultado}
             </p>
         </div>
     );
